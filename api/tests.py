0786b68bdcf0c09b60ab5063303c7def3d5e3e71
Kingsley <oparaudochukwu277@gmail.com>
2022-10-06 23:15:10 +0100

    ran some automated test

--- api/tests.py
+++ api/tests.py
@@ -1,13 +1,18 @@
 from django.test import TestCase
 import requests
 from getpass import getpass
-
+from django.contrib.auth import get_user_model
 # Create your tests here.
+User = get_user_model()
 
 class TestApiRequest(TestCase):
+    def setup(self):
+        users = User.objects.all()
+
     def test_create_user(self):
-        endpoint = 'http://localhost:8000/token'
-        username = 'udo@gmail.com'
+        endpoint = 'http://localhost:8000/token/'
+        user_input = input('Enter your username: ')
+        username = user_input
         password = getpass()
         data = {
             'username': username,
